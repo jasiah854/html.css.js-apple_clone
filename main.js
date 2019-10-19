4f05f3c026bfae256d76175a1ae9e982e5485696
jasiah854 <50156077+jasiah854@users.noreply.github.com>
2019-10-18 20:02:10 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//JavaScript
+
+const selectElement = (element) => document.querySelector(element);
+
+selectElement('.mobile-menu').addEventListener('click', () => { selectElement('header').classList.toggle('active'); });
